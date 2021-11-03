9271db9d9920256ef642ab56df78bd905e3b357a
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-03 15:43:43 +0000

    refactor the buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -12,44 +12,47 @@ class Calculator extends React.Component {
         operation: null,
       },
     };
+    this.storeObj = this.storeObj.bind(this);
   }
 /* eslint-disable*/
-  storeObj(newObj) {
+  storeObj(e) {
+    let newObj = calculate(this.state.obj, e.target.value);
     this.setState({
       obj: newObj,
     });
   }
 
-  // componentDidUpdate(){
-  //   this.value = 
-  // } 
   render() {
     return (
       <div id="keyboard">
         <input type="text" id="display" placeholder="0" value={this.state.obj.next ? this.state.obj.next : this.state.obj.total ? this.state.obj.total : 0}/>
         <br />
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, 'AC'))}>AC</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '+/-'))}>+/-</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '%'))}>%</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '/'))} className="operators">/</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '7'))}>7</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '8'))}>8</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '9'))}>9</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '*'))} className="operators">*</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '4'))}>4</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '5'))}>5</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '6'))}>6</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '-'))} className="operators">-</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '1'))}>1</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '2'))}>2</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '3'))}>3</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '+'))} className="operators">+</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '0'))} id="zero">0</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '.'))}>.</button>
-        <button type="button" onClick={() => this.storeObj(calculate(this.state.obj, '='))} className="operators">=</button>
+        <Button value="AC" storeFunc={this.storeObj}/>
+        <Button value="+/-" storeFunc={this.storeObj}/>
+        <Button value="%" storeFunc={this.storeObj}/>   
+        <Button value="/" storeFunc={this.storeObj} className="operators" />  
+        <Button value="7" storeFunc={this.storeObj}/>    
+        <Button value="8" storeFunc={this.storeObj}/> 
+        <Button value="9" storeFunc={this.storeObj}/>
+        <Button value="*" storeFunc={this.storeObj} className="operators"/>  
+        <Button value="4" storeFunc={this.storeObj}/> 
+        <Button value="5" storeFunc={this.storeObj}/> 
+        <Button value="6" storeFunc={this.storeObj}/> 
+        <Button value="-" storeFunc={this.storeObj} className="operators"/> 
+        <Button value="1" storeFunc={this.storeObj}/>        
+        <Button value="2" storeFunc={this.storeObj}/> 
+        <Button value="3" storeFunc={this.storeObj}/> 
+        <Button value="+" storeFunc={this.storeObj} className="operators"/> 
+        <Button value="0" storeFunc={this.storeObj} id="zero"/> 
+        <Button value="." storeFunc={this.storeObj}/>
+        <Button value="=" storeFunc={this.storeObj} className="operators"/>  
       </div>
     );
   }
+}
+
+function Button(props){
+  return <button type="button" onClick={props.storeFunc} value={props.value} className={props.className} id={props.id}>{props.value}</button>
 }
 /* eslint-enable */
 export default Calculator;
