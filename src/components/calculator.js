e0937b3193ddc2bb20a1e1f2158b646fc1e299d6
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-04 09:53:28 +0000

    Refactor with hook

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,85 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './calculator.css';
 import PropTypes from 'prop-types';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: {
-        total: null,
-        next: null,
-        operation: null,
-      },
-    };
-    this.storeObj = this.storeObj.bind(this);
-  }
+const Calculator = () => {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  componentDidUpdate() {
+  useEffect(() => {
     const beep = document.querySelector('#beep');
     beep.play();
+  });
+
+  let someValue = '0';
+  const { total, next } = obj;
+  if (next !== null) {
+    someValue = next;
+  } if (total !== null && next === null) {
+    someValue = total;
   }
 
-  storeObj(e) {
-    const { obj } = this.state;
-    const newObj = calculate(obj, e.target.value);
-    this.setState({
-      obj: newObj,
-    });
-  }
-
-  // }
-
-  render() {
-    let someValue = '0';
-    const { obj } = this.state;
-    const { total, next } = obj;
-    if (next !== null) {
-      someValue = next;
-    } if (total !== null && next === null) {
-      someValue = total;
-    }
-    return (
-      <div id="keyboard">
-        <input
-          type="text"
-          id="display"
-          value={someValue}
-        />
-        <br />
-        <Button value="AC" storeFunc={this.storeObj} />
-        <Button value="+/-" storeFunc={this.storeObj} />
-        <Button value="%" storeFunc={this.storeObj} />
-        <Button value="/" storeFunc={this.storeObj} className="operators" />
-        <Button value="7" storeFunc={this.storeObj} />
-        <Button value="8" storeFunc={this.storeObj} />
-        <Button value="9" storeFunc={this.storeObj} />
-        <Button value="*" storeFunc={this.storeObj} className="operators" />
-        <Button value="4" storeFunc={this.storeObj} />
-        <Button value="5" storeFunc={this.storeObj} />
-        <Button value="6" storeFunc={this.storeObj} />
-        <Button value="-" storeFunc={this.storeObj} className="operators" />
-        <Button value="1" storeFunc={this.storeObj} />
-        <Button value="2" storeFunc={this.storeObj} />
-        <Button value="3" storeFunc={this.storeObj} />
-        <Button value="+" storeFunc={this.storeObj} className="operators" />
-        <Button value="0" storeFunc={this.storeObj} id="zero" />
-        <Button value="." storeFunc={this.storeObj} />
-        <Button value="=" storeFunc={this.storeObj} className="operators" />
-      </div>
-    );
-  }
-}
+  return (
+    <div id="keyboard">
+      <input
+        type="text"
+        id="display"
+        value={someValue}
+      />
+      <br />
+      <Button value="AC" obj={obj} func={setObj} />
+      <Button value="+/-" obj={obj} func={setObj} />
+      <Button value="%" obj={obj} func={setObj} />
+      <Button value="/" obj={obj} func={setObj} className="operators" />
+      <Button value="7" obj={obj} func={setObj} />
+      <Button value="8" obj={obj} func={setObj} />
+      <Button value="9" obj={obj} func={setObj} />
+      <Button value="*" obj={obj} func={setObj} className="operators" />
+      <Button value="4" obj={obj} func={setObj} />
+      <Button value="5" obj={obj} func={setObj} />
+      <Button value="6" obj={obj} func={setObj} />
+      <Button value="-" obj={obj} func={setObj} className="operators" />
+      <Button value="1" obj={obj} func={setObj} />
+      <Button value="2" obj={obj} func={setObj} />
+      <Button value="3" obj={obj} func={setObj} />
+      <Button value="+" obj={obj} func={setObj} className="operators" />
+      <Button value="0" obj={obj} func={setObj} id="zero" />
+      <Button value="." obj={obj} func={setObj} />
+      <Button value="=" obj={obj} func={setObj} className="operators" />
+    </div>
+  );
+};
 
 const Button = (props) => {
   const {
-    storeFunc = '', value = '', className = '', id = '',
+    obj, func, value, className, id,
   } = props;
   return (
     <button
       type="button"
-      onClick={storeFunc}
+      onClick={(e) => func(calculate(obj, e.target.value))}
       value={value}
       className={className}
       id={id}
@@ -90,14 +72,16 @@ const Button = (props) => {
 };
 
 Button.defaultProps = {
-  storeFunc: null,
+  obj: null,
+  func: null,
   value: null,
   className: null,
   id: null,
 };
 
 Button.propTypes = {
-  storeFunc: PropTypes.func,
+  obj: PropTypes.objectOf,
+  func: PropTypes.func,
   value: PropTypes.string,
   className: PropTypes.string,
   id: PropTypes.string,
